05fb2f43eca139b6b4163219b8242c5011040090
Cozette Gagne <156377760+Cozette62@users.noreply.github.com>
2024-05-21 16:58:51 -0400

    Added the functionality to select

--- javascript/index.js
+++ javascript/index.js
@@ -17,16 +17,31 @@ function updateTime() {
   let parisTime = moment().tz("Europe/Paris");
 
   parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
-  parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
+  parisTimeElement.innerHTML = parisTime.format(
+    "h:mm:ss [<small>] A[</small>]"
+  );
 }
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
-  letcityTime = moment().tz(cityTimeZone);
-  console.log(cityTime.format("MMMM Do YYYY"));
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `<div class="city">
+   <div>
+      <h2>${cityName}</h2>
+    
+<div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+</div>
+<div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format(
+    "A"
+  )}</small></div>
+</div>
+`;
 }
+
 updateTime();
 setInterval(updateTime, 1000);
 
-let citiesSelect = document.querySelector("#city");
-citiesSelectElement.addEventListeneter("change", updateCity);
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
